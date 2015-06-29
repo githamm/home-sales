74cca3d7c06c074d3d4865a378bc874b68623c5e
githamm <hamm.kevin@gmail.com>
2015-06-29 12:43:16 -0600

    Add June 22 sales

--- home-geocoder.py
+++ home-geocoder.py
@@ -6,7 +6,7 @@ rows = []
 fieldnames = ['address', 'street_number direction_one street street_type direction_two suite', 'city', 'postal', 'lat', 'lng', 'Price', 'Seller_first_name', 'Seller_last_name', 'Buyer_first_name', 'Buyer_last_name', 'Date']
 
 # Change file name to be geocoded
-with open('Dubus Excel_06-15-2015.csv') as f:
+with open('Dubus Excel_06-22-2015.csv') as f:
     reader = csv.DictReader(f, delimiter=',')
     for line in reader:
         g = geocoder.mapquest(line['location'])
